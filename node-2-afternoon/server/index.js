229e872150e3ec1697284345ddf9bc43c92e0635
spiltmiruku <kitsunedg@gmail.com>
2019-11-19 15:23:23 -0700

    Finished project - tests passed in Postman

--- node-2-afternoon/server/index.js
+++ node-2-afternoon/server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mc = require('./controllers/messages_controller');
+
 const app = express();
 
 app.use(express.json());
+
+const messagesBaseUrl = '/api/messages';
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
 
 const port = 3001;
 app.listen(port, () => {
